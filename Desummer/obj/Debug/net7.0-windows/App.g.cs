1212986839420fe2dec4cb47e604f8ac9973d305
정주은 <spacernet@naver.com>
2023-11-24 16:22:52 +0900

    Add csv file
    resx파일 만들고 csv 파일 넣었음. 파일이름으로 인해 문제가 발생해서 원본파일명을 변경했음

--- Desummer/obj/Debug/net7.0-windows/App.g.cs
+++ Desummer/obj/Debug/net7.0-windows/App.g.cs
@@ -10,6 +10,7 @@
 //------------------------------------------------------------------------------
 
 using Desummer;
+using ScottPlot;
 using System;
 using System.Diagnostics;
 using System.Windows;
