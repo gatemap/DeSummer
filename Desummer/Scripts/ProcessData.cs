403aa483309aff295b2662d561660ec8599b2ae9
정주은 <spacernet@naver.com>
2023-11-24 18:23:34 +0900

    데이터 추가

---
+++ Desummer/Scripts/ProcessData.cs
@@ -0,0 +1,19 @@
+﻿namespace Desummer.Scripts
+{
+    class ProcessData
+    {
+        string data = string.Empty;
+        string csvFilePath = @"pack://application,,,/Resources";
+
+        public ProcessData()
+        {
+
+        }
+
+        public void LoadTemperatureData()
+        {
+            Uri uri = new Uri(csvFilePath + "/termalFurnace.csv", UriKind.Absolute);
+            
+        }
+    }
+}
