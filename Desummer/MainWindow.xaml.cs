93291fa4ae58915a64f5b35851c921f958d1f6ee
JiyulLe <jiyul0422@gmail.com>
2023-11-27 18:21:28 +0900

    ProcessData.cs 1주차 나누기 보기 버튼 클릭시 데이터 그리드 표시

--- Desummer/MainWindow.xaml.cs
+++ Desummer/MainWindow.xaml.cs
@@ -12,5 +12,19 @@ namespace Desummer
             InitializeComponent();
 
         }
+
+        private void Button_ShowData_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem SelectMonth = (ComboBoxItem)ComboBox_SelectMonth.SelectedValue;
+            string SelectedMonth_name = SelectMonth.Content.ToString();
+
+            ComboBoxItem SelectedWeek = (ComboBoxItem)ComboBox_SelectWeek.SelectedValue;
+            string SelectedWeek_name = SelectedWeek.Content.ToString();
+
+            ProcessData Month = new ProcessData();
+            List<TemperatureData> Select_Month = Month.SplitDataMonthly(SelectedMonth_name, SelectedWeek_name);
+
+            DataGrid_TempData.ItemsSource = Select_Month;
+        }
     }
 }
--- Desummer/Scripts/ProcessData.cs
+++ Desummer/Scripts/ProcessData.cs
@@ -10,6 +10,7 @@ namespace Desummer.Scripts
         List<TemperatureData> FebData = new List<TemperatureData>();
         List<TemperatureData> MarData = new List<TemperatureData>();
         List<TemperatureData> AprData = new List<TemperatureData>();
+        List<TemperatureData> Jan_First_Data = new List<TemperatureData>();
         public ProcessData()
         {
 
@@ -89,6 +90,59 @@ namespace Desummer.Scripts
 
             return dataList;
 
+        }
+        public List<TemperatureData> SplitDataMonthly(string month, string week)
+        {
+            data = dataResource.termalFurnace;
+
+            bool firstLine = true;
+
+            foreach (var row in data.Split('\n'))
+            {
+                // 첫째 라인에는 컬럼 헤더값이 들어있으므로 넘긴다
+                if (firstLine)
+                {
+                    firstLine = false;
+                    continue;
+                }
+
+                // 공백이 들어오면, 해당 라인을 넘긴다
+                if (string.IsNullOrEmpty(row))
+                    continue;
+
+                var columns = row.Split(',');
+
+                DateTime date = DateTime.ParseExact(columns[0], "yyyy-MM-dd H:mm", null);
+
+
+                if (date.Month == 1) { 
+                    JanData.Add(new TemperatureData(columns[0], int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3])));
+                    if (date.Day < 14)
+                    {
+                        Jan_First_Data.Add(new TemperatureData(columns[0], int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3])));
+                    }
+                }
+                else if (columns[0].Contains("2021-02"))
+                    FebData.Add(new TemperatureData(columns[0], int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3])));
+                else if (columns[0].Contains("2021-03"))
+                    MarData.Add(new TemperatureData(columns[0], int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3])));
+                else if (columns[0].Contains("2021-04"))
+                    AprData.Add(new TemperatureData(columns[0], int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3])));
+            }
+            if (month == "21년 1월")
+            {
+                if (week == "1주차")
+                {
+                    return Jan_First_Data;
+                }
+                return JanData;
+            }
+            else if (month == "21년 2월")
+                return FebData;
+            else if (month == "21년 3월")
+                return MarData;
+            else
+                return AprData;
         }
     }
 }
