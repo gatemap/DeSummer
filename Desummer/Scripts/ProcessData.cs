6007f7f6bb17255a6dfdd4aa9f310587f1d2ce79
JiyulLe <jiyul0422@gmail.com>
2023-11-28 14:42:46 +0900

    데이터 그리드 구현 month, week 선택해서 최소, 최고 온도, 평균, 정상 작동률 표시

--- Desummer/Scripts/ProcessData.cs
+++ Desummer/Scripts/ProcessData.cs
@@ -10,6 +10,7 @@ namespace Desummer.Scripts
         List<TemperatureData> FebData = new List<TemperatureData>();
         List<TemperatureData> MarData = new List<TemperatureData>();
         List<TemperatureData> AprData = new List<TemperatureData>();
+        List<TemperatureData> Week_Data = new List<TemperatureData>();
         public ProcessData()
         {
 
@@ -89,6 +90,70 @@ namespace Desummer.Scripts
 
             return dataList;
 
+        }
+        public List<TemperatureData> SplitDataMonthly(string month, string week)
+        {
+            data = dataResource.termalFurnace;
+            bool firstLine = true;
+            foreach (var row in data.Split('\n'))
+            {
+                // 첫째 라인에는 컬럼 헤더값이 들어있으므로 넘긴다
+                if (firstLine)
+                {
+                    firstLine = false;
+                    continue;
+                }
+                // 공백이 들어오면, 해당 라인을 넘긴다
+                if (string.IsNullOrEmpty(row))
+                    continue;
+                var columns = row.Split(',');
+
+                //시간 년,월,일,시,분 으로 쪼개기
+                DateTime date = DateTime.ParseExact(columns[0], "yyyy-MM-dd H:mm", null);
+
+                int Selected_month = 0;
+                switch (month)
+                {
+                    case "21년 1월":
+                        Selected_month = 1;
+                        break;
+                    case "21년 2월":
+                        Selected_month = 2;
+                        break;
+                    case "21년 3월":
+                        Selected_month = 3;
+                        break;
+                    default:
+                        Selected_month = 4;
+                        break;
+                }
+                
+                int Selected_week = 0;
+                switch (week)
+                {
+                    case "1주차":
+                        Selected_week = 7;
+                        break;
+                    case "2주차":
+                        Selected_week = 14;
+                        break;
+                    case "3주차":
+                        Selected_week = 21;
+                        break;
+                    default:
+                        Selected_week = 31;
+                        break;
+                }
+                
+                if (date.Month == Selected_month)
+                {
+                    if (date.Day <= Selected_week && date.Day > Selected_week - 7)
+                    {
+                        Week_Data.Add(new TemperatureData(columns[0], int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3])));
+                    }
+                }
+            }
+            return Week_Data;
         }
     }
 }
