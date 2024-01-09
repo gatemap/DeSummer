d6429ceb871482474215edd7a711b376a95dd896
정주은 <spacernet@naver.com>
2024-01-09 15:04:09 +0900

    Nuget package 추가
    openCV, Tesseract 추가
    ScottPlot 업데이트
    빌드 옵션 고정

--- Desummer/obj/Debug/net7.0-windows/Desummer.AssemblyInfo.cs
+++ Desummer/obj/Debug/net7.0-windows/Desummer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("CodingOn Project in Desummer")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b18637fe04845ca08b83c84acc5960d62d1f16e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desummer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desummer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
